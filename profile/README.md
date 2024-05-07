ddd1c487f8ee073aa219298ddc0604aa7ae5bb62
Andrea Esposito <esposito_andrea99@hotmail.com>
2024-05-07 12:07:37 +0200

    Enable auto update of publications
    The source for publications is the official webpage of the project on
    the IVU website: https://ivu.di.uniba.it/projects/serene.
    
    Future implementations may leverage a project-specific website.

--- profile/README.md
+++ profile/README.md
@@ -6,7 +6,8 @@ The SERENE (uSer ExpeRiENce dEtector) project, previously known as UX-SAD (User 
 
 This is a list of publications regarding SERENE.
 
+<!-- publications start -->
 1. Desolda, G., Esposito, A., Lanzilotti, R., Costabile, M.F.: Detecting Emotions Through Machine Learning for Automatic UX Evaluation. In: Ardito, C., Lanzilotti, R., Malizia, A., Petrie, H., Piccinno, A., Desolda, G., and Inkpen, K. (eds.) Human-Computer Interaction – INTERACT 2021. pp. 270–279. Springer International Publishing, Cham (2021). <https://doi.org/10.1007/978-3-030-85613-7_19>.
 2. Desolda, G., Esposito, A., Lanzilotti, R., Costabile, M.F.: Interplay between AI and HCI for UX evaluation: the SERENE case study. In: Barricelli, B.R., Fischer, G., Fogli, D., Mørch, A., Piccinno, A., and Valtolina, S. (eds.) Proceedings of the 6th International Workshop on Cultures of Participation in the Digital Age: AI for Humans or Humans for AI? (CoPDA 2022. Co-located with the International Conference on Advanced Visual Interfaces AVI 2022). pp. 55–59. CEUR-WS, Aachen (2022). <http://ceur-ws.org/Vol-3136/paper-13.pdf>
 3. Esposito, A., Desolda, G., Lanzilotti, R., Costabile, M.F.: SERENE: A Web Platform for the UX Semi-Automatic Evaluation of Website. In: Proceedings of the 2022 international conference on advanced visual interfaces. Association for Computing Machinery, New York, NY, USA (2022). <https://doi.org/10.1145/3531073.3534464>.
-
+<!-- publications end -->
---
+++ profile/update.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+import sys
+import argparse
+
+def setup_args():
+    parser = argparse.ArgumentParser(description="Update dynamic file chunks")
+    parser.add_argument("infile", type=argparse.FileType("r"), default=sys.stdin, nargs='?')
+    parser.add_argument("outfile", type=argparse.FileType("w"), default=sys.stdout, nargs='?')
+    return parser.parse_args()
+
+
+def get_publications():
+    URL = "https://ivu.di.uniba.it/projects/serene"
+
+    res = requests.get(URL)
+    page = BeautifulSoup(res.text, "html.parser")
+    target = page.find('h2',string='Publications')
+    content = []
+    for sib in target.find_next_siblings():
+        if sib.name=="h2":
+            break
+        elif sib.name != "h3" and sib.name != "p":
+            content.append(sib.findAll("li"))
+    content = [x for l in content for x in l]
+
+    publications = BeautifulSoup("<ol />", "html.parser")
+    publications.ol.append(BeautifulSoup("\n".join(str(x) for x in content), "html.parser"))
+    return publications
+    #print(page)
+
+def replace_dynamic_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        rf'<!\-\- {marker} start \-\->.*<!\-\- {marker} end \-\->',
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = f'\n{chunk}\n'
+    chunk = f'<!-- {marker} start -->{chunk}<!-- {marker} end -->'
+    return r.sub(chunk, content)
+
+
+if __name__ == "__main__":
+    args = setup_args()
+    pubs = get_publications()
+    content=args.infile.read()
+    print(replace_dynamic_chunk(content, "publications", pubs), file=args.outfile)
+    
